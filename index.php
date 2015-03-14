801fd5c6b29d9f3a63f729f03db0a0e987ff2e83
bradtracy <bradktracy@gmail.com>
2015-03-14 17:08:03 -0400

    [creating repo]

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="About Brad Tracy">
+	<meta name="author" content="Brad Tracy">
+	<link rel="shortcut icon" href="media/keyIcon.ico">
+	<link href="custom.css" rel="stylesheet">
+	<title>Bootcampdb</title>
+</head>
+
+<body>
+	<header>
+	<h2>Bootcamp DB</h2> Info about Coding Bootcamps
+	<br>
+
+	<div class="foldImage">
+		<h2>London</h2>
+		<a href="http://theironyard.com">
+        <img src="media/theironyard.png" alt="The Iron Yard" class="image"></a>
+		<form action="welcome_get.php" method="get">
+		Name: <input type="text" Location="name"><br>
+		E-mail: <input type="text" name="email"><br>
+		<input type="submit">
+		</form>
+	</div>
+
+	<div class="camp">
+		<h2>Paris</h2>
+		<p>Paris is the capital and most populous city of France.</p>
+	</div>
+
+	<div class="camp">
+		<h2>Tokyo</h2>
+		<p>Tokyo is the capital of Japan, the center of the Greater Tokyo Area,
+			and the most populous metropolitan area in the world.</p>
+		</div>
+
+</body>
+</html>
